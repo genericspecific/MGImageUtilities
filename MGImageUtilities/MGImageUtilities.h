6b80dd8219b12c043da3127994df4cf405db49f3
Constantine Karlis <dino@constantinekarlis.com>
2016-05-26 23:03:21 +0200

    Use a dynamic framework

---
+++ MGImageUtilities/MGImageUtilities.h
@@ -0,0 +1,22 @@
+//
+//  MGImageUtilities.h
+//  MGImageUtilities
+//
+//  Created by Constantine Karlis on 26/05/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MGImageUtilities.
+FOUNDATION_EXPORT double MGImageUtilitiesVersionNumber;
+
+//! Project version string for MGImageUtilities.
+FOUNDATION_EXPORT const unsigned char MGImageUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements
+
+#import <MGImageUtilities/UIImage+ProportionalFill.h>
+#import <MGImageUtilities/UIImage+Tint.h>
+
+
